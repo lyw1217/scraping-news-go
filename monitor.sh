19a0a94b07233ff9d48f5e46dd8f0bde6a9ac870
lyw1217 <mvl100d@gmail.com>
2021-11-30 11:20:19 +0900

    모니터링 스크립트 보완

--- monitor.sh
+++ monitor.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
-DIR_PATH="/home/leeyw/Documents/github/scraping-news"
+APP="scraping-news-go"
+DIR_PATH="/home/leeyw/Documents/github/${APP}"
+
+GOPATH=$DIR_PATH
+GOBIN=$DIR_PATH/bin
+
 LOG_PATH="${DIR_PATH}/log"
 LOG_NAME="nohup.log"
-VENV_PATH="${DIR_PATH}/venv/bin"
-EXE_PY="${DIR_PATH}/src/main.py"
-CMD="SCRAPER"
+
+MOD="scraping"
+EXE="${GOBIN}/${MOD}"
+CMD="GOCRAPER"
+
+export GOPATH
+export GOBIN
+
+WAIT_TIME=7
+
 # sudo check
 if [ $(id -u) -ne 0 ]; then exec sudo bash "$0" "$@"; exit; fi
 
 echo ""
-echo "--------------------------------------"
-echo "          [ SCRAPER MONITOR ]         "
-echo "--------------------------------------"
+echo " --------------------------------------"
+echo "          [ GOCRAPER MONITOR ]         "
+echo "                                with go"
+echo " --------------------------------------"
 echo ""
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
+echo " > 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -f ${CMD})
 
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+echo " 현재 구동중인 어플리케이션 pid: ${CURRENT_PID}"
 
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없음"
+if [ -z "${CURRENT_PID}" ]; then
+    echo " > 현재 구동중인 애플리케이션이 없음"
 	echo ""
 else
-	echo "> 정상 실행중..!"
-	echo "> 모니터링 종료"
+	echo " > 정상 실행중..!"
+	echo " > 모니터링 종료"
 	exit 0
 fi
 
