6b62dea39a769d3f8df7a306864dd9a6bca4af45
lyw1217 <mvl100d@gmail.com>
2022-08-01 22:12:00 +0900

    기상청 API URI

--- scraper/endpoint.go
+++ scraper/endpoint.go
@@ -5,4 +5,5 @@ const (
 	HkIssueTodayUrl string = "https://mobile.hankyung.com/apps/newsletter.view?topic=morning&gnb="
 	QuicknewsUrl    string = "https://quicknews.co.kr/"
 	HostName        string = "http://mumeog.site:9090/"
+	VilageFcstUrl   string = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst" // 기상청_단기예보 ((구)_동네예보) 조회서비스
 )
